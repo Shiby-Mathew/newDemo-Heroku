2639e54b176bc7888dfcf7f85ea4677015fa2ca4
Shiby-Mathew <shiby.ajay@gmail.com>
2023-04-26 11:57:56 +1000

    init newDemo-Heroku

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");g
+const path = require("path");
+//const { clog } = require("./middleware/clog");
+const api = require("./routes/index.js");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+//app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+
+app.use(express.static("public"));
+
+// GET Route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
